75d728fd4185bcaf85c2ff74504c80cd24340a1c
mj4721786-specxz <mj4721786@gmail.com>
2026-08-27 02:10:17 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ export async function middleware(request: NextRequest) {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get(name) {
+        get(name: string) {
           return request.cookies.get(name)?.value;
         },
         set(name: string, value: string, options: CookieOptions) {
