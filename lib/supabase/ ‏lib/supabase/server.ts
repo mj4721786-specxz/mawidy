8342f760425e69acbf5371afbbf69743d962d821
mj4721786-specxz <mj4721786@gmail.com>
2026-09-13 09:44:10 +0300

    Implement Supabase client creation in server.ts

---
+++ lib/supabase/ ‏lib/supabase/server.ts
@@ -0,0 +1,23 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+export function createClient() {
+  const cookieStore = cookies();
+  return createServerClient(
+    "https://dkfmhjgdqokxwcmlrwdv.supabase.co",
+    "sb_publishable_kKXiaiPOHk-2QqzBbRT1tQ_xiqhdAxg",
+    {
+      cookies: {
+        get(name: string) {
+          return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          cookieStore.set({ name, value, ...options });
+        },
+        remove(name: string, options: CookieOptions) {
+          cookieStore.set({ name, value: "", ...options });
+        },
+      },
+    }
+  );
+}
